13bd1c3421e62eedadc198cb7ab70f32fc3d3407
Denilson Lemus <dlemus@harding.edu>
2023-07-24 21:01:14 -0600

    ⚗️ Add button to navigate

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import tw from "twrnc";
 
 export default function App() {
   return (
     <View style={tw`flex-1 justify-center items-center`}>
       <Text>App</Text>
-      <StatusBar style="auto" />
+      <Button title="Click me!"></Button>
     </View>
   );
 }
