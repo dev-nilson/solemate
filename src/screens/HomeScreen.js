69ad46f6e5442d3f4c544dcc86fe4388fd76758c
Denilson Lemus <dlemus@harding.edu>
2023-07-25 20:37:41 -0600

    ✨ Link chat screen to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
+import { useNavigation } from "@react-navigation/native";
 import { Button, Text, View } from "react-native";
 import tw from "twrnc";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={tw`flex-1 justify-center items-center`}>
       <Text>App</Text>
-      <Button title="Click me!"></Button>
+      <Button title="Chat" onPress={() => navigation.navigate("Chat")}></Button>
     </View>
   );
 }
