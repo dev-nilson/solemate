4b9e48cd181c6c86ab2a7612bce342e84f4685d2
Denilson Lemus <dlemus@harding.edu>
2023-07-25 20:43:25 -0600

    ✨ Add login screen

--- src/StackNavigator.js
+++ src/StackNavigator.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function StackNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
     </Stack.Navigator>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import tw from "twrnc";
+
+export default function LoginScreen() {
+  return (
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text>App</Text>
+    </View>
+  );
+}
