44f7a2ec207ae2399ff58bed4050033c58f61976
Denilson Lemus <dlemus@harding.edu>
2023-07-25 20:35:00 -0600

    ✨ Add chat screen

--- src/StackNavigator.js
+++ src/StackNavigator.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,6 +8,7 @@ export default function StackNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
     </Stack.Navigator>
   );
 }
