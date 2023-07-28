5c54433a97fea42791b4be7a6e38193ca4b327d3
Denilson Lemus <dlemus@harding.edu>
2023-07-27 21:26:34 -0600

    ✨ Add background for login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <Text>App</Text>
+    <View style={tw`flex-1`}>
+      <ImageBackground
+        resizeMode="cover"
+        style={tw`flex-1`}
+        source={{
+          uri: "https://gimlet.spotifycdn.com/hero/166bdc6f-488f-4a63-8b2b-c9d4eeeab9ad/fallback.jpg",
+        }}
+      >
+        
+      </ImageBackground>
     </View>
   );
 }
