6fd4fdf930379f780a9e63a8521f97c6420a2814
Denilson Lemus <dlemus@harding.edu>
2023-07-27 21:13:37 -0600

    ♻️ Hide header for login screen

--- src/StackNavigator.js
+++ src/StackNavigator.js
@@ -17,7 +17,11 @@ export default function StackNavigator() {
           <Stack.Screen name="Chat" component={ChatScreen} />
         </>
       ) : (
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
       )}
     </Stack.Navigator>
   );
