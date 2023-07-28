7b2b033dae9d40c16ba7f0512b8864d505495eba
Denilson Lemus <dlemus@harding.edu>
2023-07-27 21:47:43 -0600

    📱 Replace view for safe view

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text, View } from "react-native";
+import { Button, SafeAreaView, Text } from "react-native";
 import tw from "twrnc";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
+    <SafeAreaView style={tw`flex-1 justify-center items-center`}>
       <Text>App</Text>
       <Button title="Chat" onPress={() => navigation.navigate("Chat")}></Button>
-    </View>
+    </SafeAreaView>
   );
 }
