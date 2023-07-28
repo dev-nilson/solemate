fefd1a4afc5559fca5ddbccc004d31fef4cd800d
Denilson Lemus <dlemus@harding.edu>
2023-07-27 21:36:17 -0600

    ✨ Add sign in button

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { ImageBackground, Text, View } from "react-native";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
 
@@ -14,7 +14,11 @@ export default function LoginScreen() {
           uri: "https://gimlet.spotifycdn.com/hero/166bdc6f-488f-4a63-8b2b-c9d4eeeab9ad/fallback.jpg",
         }}
       >
-        
+        <TouchableOpacity
+          style={tw`bg-white w-40 self-center mt-52 p-3 rounded-lg`}
+        >
+          <Text style={tw`text-center`}>Sign In</Text>
+        </TouchableOpacity>
       </ImageBackground>
     </View>
   );
