7cdded9cdd60f2743a7d3ab0ac43bd2d5b9eaba9
Denilson Lemus <dlemus@harding.edu>
2023-07-31 20:58:01 -0600

    💄 Style picture for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import { View, TouchableOpacity, Image } from "react-native";
+import tw from "twrnc";
 
 export default function Header() {
   return (
-    <View>
+    <View> 
       <TouchableOpacity>
         <Image
+          style={tw`h-10 w-10 rounded-full`}
           source={{
             uri: "https://avatarairlines.com/wp-content/uploads/2020/05/Male-placeholder-1.jpeg",
           }}
