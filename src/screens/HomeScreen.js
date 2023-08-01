66b22c47a6577f9743bfc9207c95a69482cd7bc5
Denilson Lemus <dlemus@harding.edu>
2023-07-31 21:24:00 -0600

    🎨 Format home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, SafeAreaView, Text } from "react-native";
+import { Button, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
 import Header from "../components/Header";
 
@@ -9,7 +9,6 @@ export default function HomeScreen() {
   return (
     <SafeAreaView style={{ marginTop: Constants.statusBarHeight + 10 }}>
       <Header />
-
       <Button title="Chat" onPress={() => navigation.navigate("Chat")}></Button>
     </SafeAreaView>
   );
