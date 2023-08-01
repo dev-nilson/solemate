97d787ee9c2bdda36319d19bd1c27f9804d13102
Denilson Lemus <dlemus@harding.edu>
2023-07-31 21:05:21 -0600

    💄 Fix home screen style

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, SafeAreaView, Text } from "react-native";
-import tw from "twrnc";
+import Constants from "expo-constants";
 import Header from "../components/Header";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={tw`flex-1 justify-center items-center`}>
+    <SafeAreaView style={{ marginTop: Constants.statusBarHeight + 10 }}>
       <Header />
+
       <Button title="Chat" onPress={() => navigation.navigate("Chat")}></Button>
     </SafeAreaView>
   );
