a0863fd639b58c2717028f15df0383a29f6320e6
Denilson Lemus <dlemus@harding.edu>
2023-08-02 20:50:05 -0600

    🚧 Add swiper component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { SafeAreaView } from "react-native";
 import Constants from "expo-constants";
 import Header from "../components/Header";
+import Swiper from "react-native-deck-swiper";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ marginTop: Constants.statusBarHeight + 10 }}>
       <Header />
+      <Swiper />
     </SafeAreaView>
   );
 }
