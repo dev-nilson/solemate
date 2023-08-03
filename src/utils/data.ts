22b04c1cb8a04f732aa3778b7b87975c2c97ddc6
Denilson Lemus <dlemus@harding.edu>
2023-08-02 20:50:23 -0600

    🤡 Add fake data for profiles

---
+++ src/utils/data.ts
@@ -0,0 +1,16 @@
+export const data = [
+  {
+    id: 1,
+    name: "Male Name",
+    age: 21,
+    image:
+      "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
+  },
+  {
+    id: 2,
+    name: "Female Name",
+    age: 22,
+    image:
+      "https://media.istockphoto.com/id/1074273082/vector/person-gray-photo-placeholder-woman.jpg",
+  },
+];
