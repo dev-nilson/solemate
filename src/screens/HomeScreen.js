1e02a3ae243d9d633e74ba86bce434fd2d6055b8
Denilson Lemus <dlemus@harding.edu>
2023-08-02 20:41:46 -0600

    ⚰️ Remove redundant code for home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,11 @@
-import { useNavigation } from "@react-navigation/native";
-import { Button, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import Constants from "expo-constants";
 import Header from "../components/Header";
 
 export default function HomeScreen() {
-  const navigation = useNavigation();
-
   return (
     <SafeAreaView style={{ marginTop: Constants.statusBarHeight + 10 }}>
       <Header />
-      <Button title="Chat" onPress={() => navigation.navigate("Chat")}></Button>
     </SafeAreaView>
   );
 }
