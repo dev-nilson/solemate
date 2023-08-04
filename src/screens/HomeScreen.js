940debe6d33934563c197f7f715eb64b06691193
Denilson Lemus <dlemus@harding.edu>
2023-08-03 20:53:41 -0600

    💄 Style card shadow with style sheet

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import tw from "twrnc";
-import { SafeAreaView, View, Text, Image } from "react-native";
+import { SafeAreaView, View, Text, Image, StyleSheet } from "react-native";
 import { data } from "../utils/data";
 import Constants from "expo-constants";
 import Swiper from "react-native-deck-swiper";
@@ -26,7 +26,10 @@ export default function HomeScreen() {
                 source={{ uri: card.image }}
               />
               <View
-                style={tw`absolute bottom-0 bg-white w-full h-20 justify-between items-center flex-row px-6 py-3 rounded-b-xl`}
+                style={[
+                  tw`absolute bottom-0 bg-white w-full h-20 justify-between items-center flex-row px-6 py-3 rounded-b-xl`,
+                  styles.cardShadow,
+                ]}
               >
                 <Text style={tw`text-2xl font-bold`}>{card.name}</Text>
                 <Text style={tw`text-xl font-semibold`}>{card.age}</Text>
@@ -38,3 +41,16 @@ export default function HomeScreen() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.4,
+    elevation: 2,
+  },
+});
