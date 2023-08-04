0396190d8ed1bbd3e546d5076f3cdd37f63de3a0
Denilson Lemus <dlemus@harding.edu>
2023-08-03 20:38:30 -0600

    🩹 Fix data images issue

--- src/utils/data.js
+++ src/utils/data.js
@@ -11,6 +11,6 @@ export const data = [
     name: "Female Name",
     age: 22,
     image:
-      "https://media.istockphoto.com/id/1074273082/vector/person-gray-photo-placeholder-woman.jpg",
+      "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
 ];
