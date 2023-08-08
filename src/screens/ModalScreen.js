e80bd139749d6d9297cce416ea2034d48b4c47a3
Denilson Lemus <dlemus@harding.edu>
2023-08-07 21:18:13 -0600

    ✨ Add modal component

---
+++ src/screens/ModalScreen.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import tw from "twrnc";
+
+export default function ModalScreen() {
+  return (
+    <View style={tw`flex-1 justify-center items-center`}>
+      <Text>Modal</Text>
+    </View>
+  );
+}
