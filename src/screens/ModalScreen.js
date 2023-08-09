9fb6ba09c3bdc88992deecd705b20bc3554750cb
Denilson Lemus <dlemus@harding.edu>
2023-08-08 20:56:19 -0600

    ✨ Add modal image input

--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import tw from "twrnc";
 import Constants from "expo-constants";
 
@@ -6,12 +6,19 @@ export default function ModalScreen() {
   return (
     <View style={tw`flex-1 items-center`}>
       <Text
-        style={tw`text-xl text-gray-500 font-bold mt-[${
+        style={tw`text-2xl text-gray-500 font-bold mt-[${
           Constants.statusBarHeight + 10
         }]`}
       >
         Welcome!
       </Text>
+      <Text style={tw`text-rose-500 p-4 font-bold`}>
+        Step 1: Upload a picture
+      </Text>
+      <TextInput
+        style={tw`text-center text-lg pb-2`}
+        placeholder="Image URL"
+      />
     </View>
   );
 }
