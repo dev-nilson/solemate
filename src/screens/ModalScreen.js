0c0baf072ff0a82e67af50a1ebc01afb3b3563ff
Denilson Lemus <dlemus@harding.edu>
2023-08-08 20:52:18 -0600

    ✨ Add modal header

--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
 import tw from "twrnc";
+import Constants from "expo-constants";
 
 export default function ModalScreen() {
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
-      <Text>Modal</Text>
+    <View style={tw`flex-1 items-center`}>
+      <Text
+        style={tw`text-xl text-gray-500 font-bold mt-[${
+          Constants.statusBarHeight + 10
+        }]`}
+      >
+        Welcome!
+      </Text>
     </View>
   );
 }
