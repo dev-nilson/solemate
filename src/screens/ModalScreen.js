6d4b7c8406f9673d03c06d39ded3d452552073d0
Denilson Lemus <dlemus@harding.edu>
2023-08-08 21:14:39 -0600

    ✨ Control modal inputs

--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 import Constants from "expo-constants";
 
 export default function ModalScreen() {
+  const [brand, setBrand] = useState();
+  const [model, setModel] = useState();
+  const [size, setSize] = useState();
+
   return (
     <View style={tw`flex-1 items-center`}>
       <Text
@@ -13,14 +18,26 @@ export default function ModalScreen() {
         Welcome!
       </Text>
       <Text style={tw`text-rose-500 p-4 font-bold`}>Step 1: Enter brand</Text>
-      <TextInput style={tw`text-center text-lg pb-2`} placeholder="Brand" />
+      <TextInput
+        style={tw`text-center text-lg pb-2`}
+        placeholder="Brand"
+        value={brand}
+        onChange={(text) => setBrand(text)}
+      />
       <Text style={tw`text-rose-500 p-4 font-bold`}>Step 2: Enter model</Text>
-      <TextInput style={tw`text-center text-lg pb-2`} placeholder="Model" />
+      <TextInput
+        style={tw`text-center text-lg pb-2`}
+        placeholder="Model"
+        value={model}
+        onChange={(text) => setModel(text)}
+      />
       <Text style={tw`text-rose-500 p-4 font-bold`}>Step 3: Enter size</Text>
       <TextInput
         style={tw`text-center text-lg pb-2`}
         keyboardType="numeric"
         placeholder="Size"
+        value={size}
+        onChange={(text) => setSize(text)}
       />
       <TouchableOpacity
         style={tw`w-54 p-3 rounded-lg absolute bottom-0 bg-rose-500`}
