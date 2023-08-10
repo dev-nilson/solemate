6abc5ca9fc11061a0d88e425d560acfbe8637d6b
Denilson Lemus <dlemus@harding.edu>
2023-08-09 21:23:57 -0600

    💄 Style match screen

--- src/screens/MatchScreen.js
+++ src/screens/MatchScreen.js
@@ -1,8 +1,9 @@
 import { View, Text } from "react-native";
+import tw from "twrnc";
 
 export default function MatchScreen() {
   return (
-    <View>
+    <View style={[tw`h-full bg-rose-500 pt-20`, { opacity: 0.9 }]}>
       <Text>MatchScreen</Text>
     </View>
   );
