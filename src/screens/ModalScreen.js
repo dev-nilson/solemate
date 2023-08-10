d4b942f20147ba6e03e6e07c9c84c755c4b2dd77
Denilson Lemus <dlemus@harding.edu>
2023-08-09 21:04:59 -0600

    💄 Update modal spacing

--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -19,35 +19,37 @@ export default function ModalScreen() {
       >
         Welcome!
       </Text>
-      <Text style={tw`text-rose-500 p-4 font-bold`}>Step 1: Enter brand</Text>
-      <TextInput
-        style={tw`text-center text-lg pb-2`}
-        placeholder="Brand"
-        value={brand}
-        onChange={(text) => setBrand(text)}
-        maxLength={25}
-      />
-      <Text style={tw`text-rose-500 p-4 font-bold`}>Step 2: Enter model</Text>
-      <TextInput
-        style={tw`text-center text-lg pb-2`}
-        placeholder="Model"
-        value={model}
-        onChange={(text) => setModel(text)}
-        maxLength={25}
-      />
-      <Text style={tw`text-rose-500 p-4 font-bold`}>Step 3: Enter size</Text>
-      <TextInput
-        style={tw`text-center text-lg pb-2`}
-        keyboardType="numeric"
-        placeholder="Size"
-        value={size}
-        onChange={(text) => setSize(text)}
-        maxLength={2}
-      />
+      <View style={tw`mt-10`}>
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 1: Enter brand</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          placeholder="Brand"
+          value={brand}
+          onChange={(text) => setBrand(text)}
+          maxLength={25}
+        />
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 2: Enter model</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          placeholder="Model"
+          value={model}
+          onChange={(text) => setModel(text)}
+          maxLength={25}
+        />
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 3: Enter size</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          keyboardType="numeric"
+          placeholder="Size"
+          value={size}
+          onChange={(text) => setSize(text)}
+          maxLength={2}
+        />
+      </View>
       <TouchableOpacity
         disabled={isIncomplete}
         style={[
-          tw`w-54 p-3 rounded-lg absolute bottom-0 `,
+          tw`w-54 p-3 rounded-lg absolute bottom-10 `,
           isIncomplete ? tw`bg-gray-400` : tw`bg-rose-500`,
         ]}
       >
