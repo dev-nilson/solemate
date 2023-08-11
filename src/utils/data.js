a3d79b2e3cf161c514c7914878232167466a6dd8
Denilson Lemus <dlemus@harding.edu>
2023-08-10 22:17:46 -0600

    🗃️ Extend user data

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,14 +2,35 @@ export const data = [
   {
     id: 1,
     name: "Name",
-    age: 20,
+    age: 21,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 2,
     name: "Name",
-    age: 21,
+    age: 22,
+    image:
+      "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
+  },
+  {
+    id: 3,
+    name: "Name",
+    age: 23,
+    image:
+      "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
+  },
+  {
+    id: 4,
+    name: "Name",
+    age: 24,
+    image:
+      "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
+  },
+  {
+    id: 5,
+    name: "Name",
+    age: 25,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
