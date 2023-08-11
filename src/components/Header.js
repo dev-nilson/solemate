0fecfaacf72302f34ffd09f06ef66154a49bad30
Denilson Lemus <dlemus@harding.edu>
2023-08-10 22:06:05 -0600

    💄 Update header component styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ export default function Header() {
   const navigation = useNavigation();
 
   return (
-    <View style={tw`h-12 px-3 flex-row justify-between items-center bg-[#ddd]`}>
+    <View style={tw`h-16 px-3 flex-row justify-between items-center bg-[#ddd]`}>
       <TouchableOpacity onPress={() => navigation.navigate("Match")}>
         <Image
-          style={tw`h-10 w-10 rounded-lg`}
+          style={tw`h-10 w-10 rounded-full`}
           source={{
             uri: "https://avatarairlines.com/wp-content/uploads/2020/05/Male-placeholder-1.jpeg",
           }}
@@ -23,7 +23,7 @@ export default function Header() {
         />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
-        <Icon name="chat-bubble" />
+        <Icon name="chat-bubble" color={"#f43f5e"} />
       </TouchableOpacity>
     </View>
   );
