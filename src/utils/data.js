7a8d33145980e02774d1b10ec67dd3fcc8b3c7af
Denilson Lemus <dlemus@harding.edu>
2023-08-10 22:16:40 -0600

    🗃️ Update user dats

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,15 +1,15 @@
 export const data = [
   {
     id: 1,
-    name: "Male Name",
-    age: 21,
+    name: "Name",
+    age: 20,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 2,
-    name: "Female Name",
-    age: 22,
+    name: "Name",
+    age: 21,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
