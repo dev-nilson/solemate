9b7c85cd2b6aab5516aa3d6f7365366d87084346
Denilson Lemus <dlemus@harding.edu>
2023-08-14 20:45:44 -0600

    ✨ Add text to match screen

--- src/screens/MatchScreen.js
+++ src/screens/MatchScreen.js
@@ -4,7 +4,9 @@ import tw from "twrnc";
 export default function MatchScreen() {
   return (
     <View style={[tw`h-full bg-rose-500 pt-20`, { opacity: 0.9 }]}>
-      <Text>MatchScreen</Text>
+      <Text style={tw`text-white text-center mt-5 text-lg`}>
+        You and XXX have liked each other
+      </Text>
     </View>
   );
 }
