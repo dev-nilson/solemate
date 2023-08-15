0710e58cfa636a02b016c5814f96e344ef1c8b81
Denilson Lemus <dlemus@harding.edu>
2023-08-14 20:43:49 -0600

    ♻️ Break down modal screen

---
+++ src/components/Form.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import tw from "twrnc";
+
+export default function Form() {
+  const [brand, setBrand] = useState();
+  const [model, setModel] = useState();
+  const [size, setSize] = useState();
+
+  const isIncomplete = !brand || !model || !size;
+
+  return (
+    <View>
+      <View style={tw`mt-10`}>
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 1: Enter brand</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          placeholder="Brand"
+          value={brand}
+          onChange={(text) => setBrand(text)}
+          maxLength={25}
+        />
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 2: Enter model</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          placeholder="Model"
+          value={model}
+          onChange={(text) => setModel(text)}
+          maxLength={25}
+        />
+        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 3: Enter size</Text>
+        <TextInput
+          style={tw`text-center text-lg pb-2`}
+          keyboardType="numeric"
+          placeholder="Size"
+          value={size}
+          onChange={(text) => setSize(text)}
+          maxLength={2}
+        />
+      </View>
+      <TouchableOpacity
+        disabled={isIncomplete}
+        style={[
+          tw`w-54 p-3 rounded-lg absolute bottom-10 `,
+          isIncomplete ? tw`bg-gray-400` : tw`bg-rose-500`,
+        ]}
+      >
+        <Text style={tw`text-center text-white font-bold text-lg`}>Submit</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- src/screens/ModalScreen.js
+++ src/screens/ModalScreen.js
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import { Text, TextInput, View, TouchableOpacity } from "react-native";
+import { Text, View } from "react-native";
 import tw from "twrnc";
 import Constants from "expo-constants";
+import Form from "../components/Form";
 
 export default function ModalScreen() {
-  const [brand, setBrand] = useState();
-  const [model, setModel] = useState();
-  const [size, setSize] = useState();
-
-  const isIncomplete = !brand || !model || !size;
-
   return (
     <View style={tw`flex-1 items-center`}>
       <Text
@@ -19,42 +13,7 @@ export default function ModalScreen() {
       >
         Welcome!
       </Text>
-      <View style={tw`mt-10`}>
-        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 1: Enter brand</Text>
-        <TextInput
-          style={tw`text-center text-lg pb-2`}
-          placeholder="Brand"
-          value={brand}
-          onChange={(text) => setBrand(text)}
-          maxLength={25}
-        />
-        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 2: Enter model</Text>
-        <TextInput
-          style={tw`text-center text-lg pb-2`}
-          placeholder="Model"
-          value={model}
-          onChange={(text) => setModel(text)}
-          maxLength={25}
-        />
-        <Text style={tw`text-rose-500 p-4 font-bold`}>Step 3: Enter size</Text>
-        <TextInput
-          style={tw`text-center text-lg pb-2`}
-          keyboardType="numeric"
-          placeholder="Size"
-          value={size}
-          onChange={(text) => setSize(text)}
-          maxLength={2}
-        />
-      </View>
-      <TouchableOpacity
-        disabled={isIncomplete}
-        style={[
-          tw`w-54 p-3 rounded-lg absolute bottom-10 `,
-          isIncomplete ? tw`bg-gray-400` : tw`bg-rose-500`,
-        ]}
-      >
-        <Text style={tw`text-center text-white font-bold text-lg`}>Submit</Text>
-      </TouchableOpacity>
+      <Form />
     </View>
   );
 }
