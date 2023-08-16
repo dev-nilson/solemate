df5804d4799a5375b8b2fce9f48cf4b0d9986de2
Denilson Lemus <dlemus@harding.edu>
2023-08-15 21:54:21 -0600

    💄 Updat topbar styles

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,8 +1,9 @@
 import { View, Text } from "react-native";
+import tw from "twrnc";
 
 export default function Topbar({ title }) {
   return (
-    <View>
+    <View style={tw`h-16 px-3 flex-row justify-between items-center`}>
       <Text>{title}</Text>
     </View>
   );
