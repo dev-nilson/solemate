044ac2bda76dd9928b3e28b805ed2d0ab84b0b0b
Denilson Lemus <dlemus@harding.edu>
2023-08-15 22:02:35 -0600

    ✨ Add topbar navigation functionality

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,12 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Icon } from "@rneui/themed";
 import tw from "twrnc";
 
 export default function Topbar({ title }) {
+  const navigation = useNavigation();
+
   return (
     <View style={tw`h-16 px-3 flex-row justify-between items-center`}>
       <View style={tw`flex flex-row items-center gap-3`}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
           <Icon name="arrow-back" color={"#555"} size={36} />
         </TouchableOpacity>
         <Text style={tw`text-xl font-bold`}>{title}</Text>
