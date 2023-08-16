d5b80b7d2372c6b896509fcada551371b4518f73
Denilson Lemus <dlemus@harding.edu>
2023-08-15 22:00:40 -0600

    💄 Update topbar styles

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -5,11 +5,11 @@ import tw from "twrnc";
 export default function Topbar({ title }) {
   return (
     <View style={tw`h-16 px-3 flex-row justify-between items-center`}>
-      <View>
+      <View style={tw`flex flex-row items-center gap-3`}>
         <TouchableOpacity>
           <Icon name="arrow-back" color={"#555"} size={36} />
         </TouchableOpacity>
-        <Text>{title}</Text>
+        <Text style={tw`text-xl font-bold`}>{title}</Text>
       </View>
     </View>
   );
