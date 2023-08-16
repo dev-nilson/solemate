a2a8216343c01912d4a2ca6e6fb6f8658c7eea09
Denilson Lemus <dlemus@harding.edu>
2023-08-15 21:58:02 -0600

    ✨ Add navigation button for topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Icon } from "@rneui/themed";
 import tw from "twrnc";
 
 export default function Topbar({ title }) {
   return (
     <View style={tw`h-16 px-3 flex-row justify-between items-center`}>
-      <Text>{title}</Text>
+      <View>
+        <TouchableOpacity>
+          <Icon name="arrow-back" color={"#555"} size={36} />
+        </TouchableOpacity>
+        <Text>{title}</Text>
+      </View>
     </View>
   );
 }
