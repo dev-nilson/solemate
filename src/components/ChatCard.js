f52a3a35f511d4844bc45e05cdc86422d3df5be9
Denilson Lemus <dlemus@harding.edu>
2023-08-16 20:40:18 -0600

    🐛 Fix card image not showing

--- src/components/ChatCard.js
+++ src/components/ChatCard.js
@@ -1,9 +1,13 @@
 import { TouchableOpacity, Text, Image } from "react-native";
+import tw from "twrnc";
 
 export default function ChatCard({ item }) {
   return (
     <TouchableOpacity>
-      <Image source={{ uri: item.image }} />
+      <Image
+        style={tw`rounded-full h-16 w-16 mr-4`}
+        source={{ uri: item.image }}
+      />
       <Text>{item.name}</Text>
     </TouchableOpacity>
   );
