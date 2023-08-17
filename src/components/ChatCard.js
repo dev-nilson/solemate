a7ef7ad42cfad9287aae2b515f04de589b24c3b2
Denilson Lemus <dlemus@harding.edu>
2023-08-16 20:49:20 -0600

    💄 Add shadows to chat card

--- src/components/ChatCard.js
+++ src/components/ChatCard.js
@@ -1,10 +1,13 @@
-import { TouchableOpacity, Text, Image } from "react-native";
+import { TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 import tw from "twrnc";
 
 export default function ChatCard({ item }) {
   return (
     <TouchableOpacity
-      style={tw`flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg`}
+      style={[
+        tw`flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg`,
+        styles.cardShadow,
+      ]}
     >
       <Image
         style={tw`rounded-full h-16 w-16 mr-4`}
@@ -14,3 +17,16 @@ export default function ChatCard({ item }) {
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.4,
+    elevation: 2,
+  },
+});
