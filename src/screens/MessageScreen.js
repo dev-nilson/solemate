d77c31436b659d3dd4aa1a9a3393fc86f67ab3d9
Denilson Lemus <dlemus@harding.edu>
2023-08-17 21:34:16 -0600

    💄 Display message screen in a safe area

--- src/screens/MessageScreen.js
+++ src/screens/MessageScreen.js
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, Text } from "react-native";
+import Constants from "expo-constants";
 import Topbar from "../components/Topbar";
 
 export default function MessageScreen() {
   return (
-    <View>
+    <SafeAreaView
+      style={{ marginTop: Constants.statusBarHeight + 10, flex: 1 }}
+    >
       <Topbar title="Chat" showActions />
       <Text>MessageScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 }
