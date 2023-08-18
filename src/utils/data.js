9930ed4e4c141000eb33625d63dc628ce5720827
Denilson Lemus <dlemus@harding.edu>
2023-08-17 21:44:12 -0600

    ⚗️ Experiment with different names

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,70 +1,70 @@
 export const data = [
   {
     id: 1,
-    name: "Name",
+    name: "Name One",
     age: 21,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 2,
-    name: "Name",
+    name: "Name Two",
     age: 22,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 3,
-    name: "Name",
+    name: "Name Three",
     age: 23,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 4,
-    name: "Name",
+    name: "Name Four",
     age: 24,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 5,
-    name: "Name",
+    name: "Name Five",
     age: 25,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 6,
-    name: "Name",
+    name: "Name Six",
     age: 26,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 7,
-    name: "Name",
+    name: "Name Seven",
     age: 27,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 8,
-    name: "Name",
+    name: "Name Eight",
     age: 28,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 9,
-    name: "Name",
+    name: "Name Nine",
     age: 29,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
   },
   {
     id: 10,
-    name: "Name",
+    name: "Name Ten",
     age: 30,
     image:
       "https://st4.depositphotos.com/9998432/25177/v/600/depositphotos_251778046-stock-illustration-person-gray-photo-placeholder-man.jpg",
